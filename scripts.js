1f346c4c3b690517fb26f033f7c7575338051a3e
BMasilo <40583969+BMasilo@users.noreply.github.com>
2019-05-07 10:44:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// selecting all elements from the document with class ='memory-card'
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
@@ -6,7 +7,7 @@ let firstCard, secondCard;
 function flipCard() {
   this.classList.add('flip');
 
-  if(!hasFlippedCard){
+  if(hasFlippedCard != true){
     hasFlippedCard = true;
     firstCard = this;
   } else{
@@ -15,10 +16,14 @@ function flipCard() {
 
     // do cards match?
     (firstCard.dataset.framework === secondCard.dataset.framework) ? 
-    (firstCard.removeEventListener('click', flipCard), secondCard.removeEventListener('click', flipCard)) : (firstCard.classList.remove('flip'), secondCard.classList.remove('flip')) 
+    //if they match
+    (firstCard.removeEventListener('click', flipCard), secondCard.removeEventListener('click', flipCard)) : 
+    //if they don't match
+    (firstCard.classList.remove('flip'), secondCard.classList.remove('flip')) 
   }
 }
 
+//to shuffle cards when the page loads
 (function newBoard(){
   cards.forEach(card => {
     card.style.order = Math.floor(Math.random() * 12);
